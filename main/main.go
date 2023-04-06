d1f4d73f2dfb8f43d8ba5a71bc52fc3bae1c27d0
Yevgeny Khessin <yev@dimo.zone>
2023-04-06 14:19:19 -0400

    Cleaning up word lists

--- main/main.go
+++ main/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"encoding/hex"
+	"crypto/rand"
 	"fmt"
 
 	"github.com/DIMO-Network/go-mnemonic"
 )
 
 func main() {
-	s := "0xd744468B9192301650f8Cb5e390BdD824DFA6Dd9"
 
-	data, err := hex.DecodeString(s)
-	if err != nil {
-		panic(err)
-	}
-	words, _ := mnemonic.EntropyToMnemonic(data)
+	token := make([]byte, 20)
+	rand.Read(token)
+
+	words, _ := mnemonic.EntropyToMnemonic(token)
 	fmt.Println(words)
+
+	threeWords, _ := mnemonic.EntropyToMnemonicThreeWords(token)
+	fmt.Println(threeWords)
 }
